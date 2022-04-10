b988afb2931e2e7dad1bc4886488001fbbe21f96
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2022-04-10 04:49:49 -0400

    added robots.txt, head, global lang,  and alt tag for img

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -1,10 +1,12 @@
 import { motion } from "framer-motion"
+import Head from "next/head"
 
 export default function Hero() {
   return (
     <div className="flex flex-col items-center justify-center mt-10">
+      
 
-        <motion.img initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: .8, duration: .3 }} className=" rounded-xl w-10/12 max-w-[700px]" layout='responsive' src='/sessi.jpg' ></motion.img>
+        <motion.img initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: .8, duration: .3 }} className=" rounded-xl w-10/12 max-w-[700px]" layout='responsive' src='/sessi.jpg' alt='Sessi Blanchard portrait'></motion.img>
 
         
         
@@ -25,7 +27,7 @@ export default function Hero() {
 <br/> <br/>As an organizer, Sessi co-founded and currently serves as the lead organizer of the Socialist Drug Politics Organizing Committee (SDPOC), an affiliate of the NYC Democratic Socialists of America. Additionally, she co-founded and formerly served as the lead organizer of Do It Safe, Heaux! (DISH!), a discontinued harm reduction collective by and for trans women who use crystal methamphetamine. 
 <br/> <br/>As a journalist, she has published with <i>The Intercept, VICE, The Appeal,</i> and <i>ACLU Magazine</i>, among others. From 2018 to 2021, she was the staff writer at <i>FILTER</i>, one of the only online magazines dedicated to covering harm reduction. She also had a <i>VICE</i> sex column,<i> MTF & DTF</i>.  Sessi and her journalism has attracted the media attention of CBC, The Brian Lehrer Show, and even the far-right radio host Ben Shapiro.
 <br/> <br/>As a harm reductionist, Sessi works as a peer outreach work at the Lower East Side Harm Reduction Center. There, she provides harm reduction counseling as a Certified Peer Recovery Advocate, operates the exchange, and builds meaningful relationships with people who use drugs through street outreach. 
-<br/> <br/>Before all of this, Sessi graduated <i>cum</i> laude from Vassar College with degrees in philosophy and women’s studies.
+<br/> <br/>Before all of this, Sessi graduated <i>cum laude</i> from Vassar College with degrees in philosophy and women’s studies.
 
         </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,20 @@ import Hero from './components/hero'
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-top py-2 mt-4">
+      
       <Head>
-
+        <title>Sessi Blanchard</title>
+        <meta name="Sessi Blanchard" content="Sessi Blanchard Homepage" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      
       <Navbar />
       <Hero />
 
       
     </div>
+    
   )
 }
 
