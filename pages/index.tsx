77881793aa237aa94e7ead0b686309485f8d86bd
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2022-06-17 01:03:13 -0400

    added twitter link

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Hero from './components/hero'
 
+
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-top py-2 mt-4">
@@ -13,7 +14,8 @@ const Home: NextPage = () => {
       </Head>
 
       <Hero />
-      {/* Footer */}
+      <a href='https://twitter.com/SessiBlanchard'><img src='/twitter.png' width='50px' height='50px' className='mb-20'></img></a>
+
     </div>
   )
 }
