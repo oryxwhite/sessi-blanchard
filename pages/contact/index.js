7ef544629b90cc5b53f669c1aa9b0f50652ae450
oryxwhite <93627129+oryxwhite@users.noreply.github.com>
2022-06-21 07:16:05 -0400

    name field

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -43,7 +43,7 @@ export default function Contact() {
           </label>
         </div>
         <div className="">
-          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="name" type="text" >
+        <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="name" type="text" >
           </input>
         </div>
       </div>
